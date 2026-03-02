1a6ee06ac7db9e558c4c95f91f04e24f90a1c633
Aldiss1 <aldisuryasaputra693@gmail.com>
2026-03-03 05:51:11 +0700

    JS5_Praktikum 2 Penerapan Layouting

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -1,17 +1,22 @@
-<!-- Content Header (Page header) -->
-<div class="content-header">
+<section class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">@yield('title', 'Blank Page')</h1>
+                <h1>{{ isset($breadcrumb) ? $breadcrumb->title : '' }}</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="/">Home</a></li>
-                    <li class="breadcrumb-item active">@yield('title', 'Blank Page')</li>
+                    @if(isset($breadcrumb))
+                    @foreach($breadcrumb->list as $key => $value)
+                    @if($key == count($breadcrumb->list) - 1)
+                    <li class="breadcrumb-item active">{{ $value }}</li>
+                    @else
+                    <li class="breadcrumb-item">{{ $value }}</li>
+                    @endif
+                    @endforeach
+                    @endif
                 </ol>
             </div>
         </div>
     </div>
-</div>
-<!-- /.content-header -->
+</section>
